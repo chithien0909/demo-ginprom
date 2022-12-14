5b8f7a286806e608d507588a382d7cf6a1c8c1f4
chithien0909 <chithien0909@gmail.com>
2022-12-15 01:04:45 +0700

    ignore route

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import (
 	"github.com/Depado/ginprom"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func main() {
 	r := gin.Default()
+
+	registry := prometheus.NewRegistry() // creates new prometheus metric registry
+
 	p := ginprom.New(
+		ginprom.Registry(registry),
 		ginprom.Engine(r),
-		ginprom.Subsystem("gin"),
-		ginprom.Path("/metrics"),
+		ginprom.Subsystem("ginprom"),
+		ginprom.Namespace("my_prometheus"),
+		ginprom.Path("/api/metrics"),
+		ginprom.Ignore("/api/no/no/no", "/hello/:id"),
+		ginprom.Token("supersecrettoken"),
 	)
 	p.AddCustomGauge("custom", "Some help text to provide", []string{"label"})
 	r.Use(p.Instrument())
